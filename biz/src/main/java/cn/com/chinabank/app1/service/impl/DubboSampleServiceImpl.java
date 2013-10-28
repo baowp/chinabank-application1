8024daa3f8838871b342a99d0e54530e0ba8583b
baowp <122515909@qq.com>
2013-10-28 13:36:32 +0800

    a

--- biz/src/main/java/cn/com/chinabank/app1/service/impl/DubboSampleServiceImpl.java
+++ biz/src/main/java/cn/com/chinabank/app1/service/impl/DubboSampleServiceImpl.java
@@ -2,11 +2,9 @@ package cn.com.chinabank.app1.service.impl;
 
 import java.io.Serializable;
 
-import org.springframework.stereotype.Service;
-
 import cn.com.chinabank.shared.service.DubboSampleService;
 
-@Service("dubboSampleService")
+@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
 public class DubboSampleServiceImpl implements DubboSampleService {
 
 	private final org.apache.log4j.Logger logger = org.apache.log4j.Logger
