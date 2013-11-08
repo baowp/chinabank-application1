8096c33f0c7944ac4e64897185708e7a93a6f5b8
baowp <122515909@qq.com>
2013-11-08 21:15:47 +0800

    a

---
+++ web/src/test/java/cn/com/chinabank/app1/test/Launch.java
@@ -0,0 +1,16 @@
+package cn.com.chinabank.app1.test;
+
+import cn.com.chinabank.shared.launch.Main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baowp
+ * Date: 11/7/13
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Launch {
+    public static void main(String args[]) {
+        Main.main(args);
+    }
+}
