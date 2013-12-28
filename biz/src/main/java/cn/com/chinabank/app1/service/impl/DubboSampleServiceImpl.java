145f6f0c1f53ab5a8fd947187a7d2a130ec8d51a
baowp <122515909@qq.com>
2013-12-28 16:08:03 +0800

    modified

--- biz/src/main/java/cn/com/chinabank/app1/service/impl/DubboSampleServiceImpl.java
+++ biz/src/main/java/cn/com/chinabank/app1/service/impl/DubboSampleServiceImpl.java
@@ -1,20 +1,27 @@
 package cn.com.chinabank.app1.service.impl;
 
-import java.io.Serializable;
-
+import cn.com.chinabank.shared.dto.BookDto;
 import cn.com.chinabank.shared.service.DubboSampleService;
+
+import java.io.Serializable;
 
 @com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
 public class DubboSampleServiceImpl implements DubboSampleService {
 
-	private final org.apache.log4j.Logger logger = org.apache.log4j.Logger
-			.getLogger(this.getClass());
+    private final org.apache.log4j.Logger logger = org.apache.log4j.Logger
+            .getLogger(this.getClass());
 
-	@Override
-	public Serializable execute(String s) {
-		String str = "application1 executed: " + s;
-		logger.info(str);
-		return str;
-	}
+    @Override
+    public Serializable execute(String s) {
+        String str = "application1 executed: " + s;
+        logger.info(str);
+        return str;
+    }
+
+    @Override
+    public Object execute(BookDto book) {
+        logger.info(String.format("application1 executed: %1$s", book));
+        return String.format("[%1$s]", book);
+    }
 
 }
